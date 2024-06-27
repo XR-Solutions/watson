cf5bf0a5b1a0662ac9942bb519621b3f5d510c1e
Brandon <brandonplokker@live.nl>
2024-06-27 12:29:04 +0200

    feat(chat): client call to get chat history

---
+++ src/Watson.Mobile.Client/Services/Endpoints/ChatService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Watson.Mobile.Client.Http;
+using Watson.Mobile.Client.Models;
+using Watson.Mobile.Client.Options;
+
+namespace Watson.Mobile.Client.Services.Endpoints
+{
+    public class ChatService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ChatService(IOptions<ApiSettings> apiSettings)
+        {
+            var handler = new BypassSslValidationHandler();
+            _httpClient = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(apiSettings.Value.BaseUrl)
+            };            
+        }
+
+        public async Task<object> GetChatHistoryAsync()
+        {
+            var response = await _httpClient.GetAsync("/api/v1/chat/messages");
+            var json = await response.Content.ReadAsStringAsync();
+            Response<object> wrapper = JsonConvert.DeserializeObject<Response<object>>(json);
+
+            return wrapper.Data;
+        }
+    }
+}
